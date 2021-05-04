33bb97a1e5a261aa1d2c4800eabdbf8b32d9cb24
Jozef Kudrys <jskudrys@gmail.com>
2021-05-04 18:07:58 +0200

    now it is possible to input your own functions

--- WiredTranssmision/Form1.Designer.cs
+++ WiredTranssmision/Form1.Designer.cs
@@ -29,32 +29,35 @@ namespace WiredTranssmision
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.Spectrum = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.FunctionChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.Spectrum)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.FunctionChart)).BeginInit();
             this.SuspendLayout();
             // 
             // Spectrum
             // 
-            chartArea1.AxisX.Interval = 100D;
-            chartArea1.Name = "ChartArea1";
-            this.Spectrum.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.Spectrum.Legends.Add(legend1);
+            chartArea5.AxisX.Interval = 100D;
+            chartArea5.Name = "ChartArea1";
+            this.Spectrum.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.Spectrum.Legends.Add(legend5);
             this.Spectrum.Location = new System.Drawing.Point(12, 296);
             this.Spectrum.Name = "Spectrum";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
-            this.Spectrum.Series.Add(series1);
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            series5.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.Spectrum.Series.Add(series5);
             this.Spectrum.Size = new System.Drawing.Size(614, 306);
             this.Spectrum.TabIndex = 0;
             this.Spectrum.Text = "chart1";
@@ -62,26 +65,56 @@ namespace WiredTranssmision
             // 
             // FunctionChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.FunctionChart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.FunctionChart.Legends.Add(legend2);
-            this.FunctionChart.Location = new System.Drawing.Point(25, 12);
+            chartArea6.Name = "ChartArea1";
+            this.FunctionChart.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.FunctionChart.Legends.Add(legend6);
+            this.FunctionChart.Location = new System.Drawing.Point(598, 308);
             this.FunctionChart.Name = "FunctionChart";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.FunctionChart.Series.Add(series2);
-            this.FunctionChart.Size = new System.Drawing.Size(601, 278);
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.FunctionChart.Series.Add(series6);
+            this.FunctionChart.Size = new System.Drawing.Size(565, 278);
             this.FunctionChart.TabIndex = 1;
             this.FunctionChart.Text = "chart2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(425, 155);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(372, 67);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 186);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(113, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1175, 625);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.FunctionChart);
             this.Controls.Add(this.Spectrum);
             this.Name = "Form1";
@@ -90,6 +123,7 @@ namespace WiredTranssmision
             ((System.ComponentModel.ISupportInitialize)(this.Spectrum)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.FunctionChart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -97,6 +131,9 @@ namespace WiredTranssmision
 
         private System.Windows.Forms.DataVisualization.Charting.Chart Spectrum;
         private System.Windows.Forms.DataVisualization.Charting.Chart FunctionChart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- WiredTranssmision/Form1.cs
+++ WiredTranssmision/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using AForge.Math;
+using csscript;
+using CSScriptLib;
 using System.Runtime.InteropServices;
 
 namespace WiredTranssmision
@@ -21,15 +23,53 @@ namespace WiredTranssmision
         List<double> functionXValues = new List<double>();
         List<double> functionYValues = new List<double>();
 
+        string functionPeriod = "";
+        string functionScript = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
 
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Spectrum.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+
+            // Spectrum.ChartAreas[0].AxisX.Interval = 0;
+            
+        }
+
+        private void chart1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                Spectrum.ChartAreas[0].AxisX.Interval += 1;
+            }
+            else if (e.Delta < 0)
+            {
+                if (Spectrum.ChartAreas[0].AxisX.Interval - 1.0 > 0)
+                {
+                    Spectrum.ChartAreas[0].AxisX.Interval -= 1;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             this.MouseWheel += new MouseEventHandler(chart1_MouseWheel);
+
+            dynamic funcScript = CSScript.Evaluator.LoadMethod(@"double func(double t)
+                                                           {" + functionScript + "}");
 
             double func(double t)
             {
-                return 10 * Math.Sin(200 * Math.PI * t) + 10 * Math.Sin(800 * Math.PI * t) + 10 * Math.Sin(2000 * Math.PI * t);
+                return 4 * Math.Sin(2 * Math.PI * t) * Math.Sin(2 * Math.PI * t);
+                //return 10 * Math.Sin(200 * Math.PI * t) + 10 * Math.Sin(800 * Math.PI * t) + 10 * Math.Sin(2000 * Math.PI * t);
                 //return 10 * Math.Sin(100 * t) + 10 * Math.Sin(400 * t) + 10 * Math.Sin(1000 * t);
                 /*
                 if(Math.Sin(t * Math.PI * 2) > 0)
@@ -48,21 +88,25 @@ namespace WiredTranssmision
 
             //FOURIER
 
-            const double period = 1.0/100.0;
-            const double intervals = 1;
+            dynamic funcPeriodScript = CSScript.Evaluator.LoadMethod(@"double period()
+                                                                     {" + functionPeriod + "}");
+
+            double period = funcPeriodScript.period();
+            const double intervals = 2;
             const int howMany = 2048;
 
+            
             Complex[] data = new Complex[howMany];
 
             for (int i = 0; i < howMany; ++i)
             {
-                double functionResult = func(period * intervals * (i / Convert.ToDouble(howMany)));
+                double functionResult = funcScript.func(period * intervals * (i / Convert.ToDouble(howMany)));
                 data[i] = new Complex(functionResult, 0);
             }
 
-            for (double i = period / 100; i < period * intervals; i += period / 100)
+            for (double i = period / 100.0; i < period * intervals; i += period / 100.0)
             {
-                functionYValues.Add(func(i));
+                functionYValues.Add(funcScript.func(i));
                 functionXValues.Add(i);
             }
 
@@ -84,40 +128,24 @@ namespace WiredTranssmision
 
             double sr = howMany / (period * intervals);
 
-            for (int i = 1; i < 100; ++i)
+            for (int i = 0; i < 100; ++i)
             {
                 spectrumYValues.Add(bins[i] * 2);
                 spectrumXValues.Add((double)i * sr / howMany);
             }
-        }
 
-        private void chart1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
             Spectrum.Series["Series1"].Points.DataBindXY(spectrumXValues, spectrumYValues);
-            Spectrum.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
-
-            // Spectrum.ChartAreas[0].AxisX.Interval = 0;
             FunctionChart.Series["Series1"].Points.DataBindXY(functionXValues, functionYValues);
         }
 
-        private void chart1_MouseWheel(object sender, MouseEventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if (e.Delta > 0)
-            {
-                Spectrum.ChartAreas[0].AxisX.Interval += 1;
-            }
-            else if (e.Delta < 0)
-            {
-                if (Spectrum.ChartAreas[0].AxisX.Interval - 1.0 > 0)
-                {
-                    Spectrum.ChartAreas[0].AxisX.Interval -= 1;
-                }
-            }
+            functionScript = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            functionPeriod = textBox2.Text;
         }
     }
 }
